d7b2a7c5acb758eba6fa618018ffc8f014adf98a
lengyuetao <dyadan@163.com>
2018-01-11 10:53:03 +0800

    rabbit配置类

--- src/main/java/com/example/sprinboot_mq/msg/RabbitConfig.java
+++ src/main/java/com/example/sprinboot_mq/msg/RabbitConfig.java
@@ -4,8 +4,12 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 用来配置队列、交换器、路由等高级信息
+ */
 @Configuration
 public class RabbitConfig {
+
     @Bean
     public Queue heloQueue(){
         return new Queue("hello");
